991372e30e6e01b91b8998f7b88f33e183210bb5
Huang Runhua <45262463+HuangRunHua@users.noreply.github.com>
2022-01-23 17:16:50 +0800

    Initial Commit

---
+++ OpenAndSaveImageTest/OpenAndSaveImageTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenAndSaveImageTestApp.swift
+//  OpenAndSaveImageTest
+//
+//  Created by Runhua Huang on 2022/1/23.
+//
+
+import SwiftUI
+
+@main
+struct OpenAndSaveImageTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
