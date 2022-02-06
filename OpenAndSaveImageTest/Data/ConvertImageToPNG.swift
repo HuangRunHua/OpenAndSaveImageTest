7302285f049c5d8a87de595603876eeb2d514690
Huang Runhua <45262463+HuangRunHua@users.noreply.github.com>
2022-02-06 11:41:31 +0800

    删除无用的代码

--- OpenAndSaveImageTest/Data/ConvertImageToPNG.swift
+++ OpenAndSaveImageTest/Data/ConvertImageToPNG.swift
@@ -34,25 +34,5 @@ struct ImageProcess {
         savePanel.nameFieldLabel = "File name:"
         let response = savePanel.runModal()
         return response == .OK ? savePanel.url : nil
-    }
-    
-//    func saveImage(view: View) {
-//        // let view = CoverImageView().environmentObject(imageData)
-//        let imageData = view.asPngData(rect: CGRect.init(x: 0, y: 0, width: 1024, height: 768))
-//        if let url = saveURL() {
-//            try? imageData!.write(to: url)
-//        }
-//         // print(imageData)
-//    }
-//    
-//    func saveImageToApp(data: Data) {
-//        let documents = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
-//        let url = documents.appendingPathComponent("example.png")
-//        do {
-//            try data.write(to: url)
-//        } catch {
-//            print("Unable to Write Image Data to Disk")
-//        }
-//    }
-    
+    }    
 }
